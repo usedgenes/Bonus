5cdbbfaa33b3915a083a673411a39c5ae17990a3
Eugene <eugeneli126@gmail.com>
2025-09-26 21:50:34 -0400

    Initial Commit

---
+++ Bonus/BonusApp.swift
@@ -0,0 +1,17 @@
+//
+//  BonusApp.swift
+//  Bonus
+//
+//  Created by Eugene on 9/26/25.
+//
+
+import SwiftUI
+
+@main
+struct BonusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bonus/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Bonus
+//
+//  Created by Eugene on 9/26/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
